23c6def12e357fd28cbdfb608626fd37ecd78792
Chanrattnak Mong <2023002mong@aupp.edu.kh>
2024-12-06 23:26:48 +0700

    successfully create submodule for delivery

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,9 +6,10 @@ import { UsersModule } from './users/users.module';
 import { MenuModule } from './menu/menu.module';
 import { RestaurantModule } from './restaurant/restaurant.module';
 import { OrdersModule } from './orders/orders.module';
+import { DeliveryModule } from './delivery/delivery.module';
 
 @Module({
-  imports: [FoodModule, UsersModule, MenuModule, RestaurantModule, OrdersModule],
+  imports: [FoodModule, UsersModule, MenuModule, RestaurantModule, OrdersModule, DeliveryModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/delivery/dto/create-delivery.dto.ts
@@ -0,0 +1,7 @@
+export class CreateDeliveryDto {
+    orderId: number;
+    status: string;
+    deliveryAddress: string;
+    deliveryTime: string;
+  }
+  
